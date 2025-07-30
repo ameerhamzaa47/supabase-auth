a6452b56a62ffec0ce1ca298489552baf99686a4
Ameer Hamza <hamzaataariq12@gmail.com>
2025-07-30 10:00:21 +0500

    commit

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -3,11 +3,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://xoaviixgmtshuuliugtr.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvYXZpaXhnbXRzaHV1bGl1Z3RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3OTk2NzAsImV4cCI6MjA2OTM3NTY3MH0.kbptfbg4tn1mTtaJExVwlMsACcEzY0Nt0hC0v1rVTQ8'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
-
-// const supabaseUrl = 'https://xoaviixgmtshuuliugtr.supabase.co'
-// const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvYXZpaXhnbXRzaHV1bGl1Z3RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3OTk2NzAsImV4cCI6MjA2OTM3NTY3MH0.kbptfbg4tn1mTtaJExVwlMsACcEzY0Nt0hC0v1rVTQ8'
